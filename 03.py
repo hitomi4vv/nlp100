e2dda360f3d253122d6e969c98d1db6e729464d2
Hitomi <hitomi4vv@outlook.jp>
2018-02-04 03:08:04 +0900

    Remove comments

--- 03.py
+++ 03.py
@@ -1,8 +1,3 @@
-'''
-第1章: 準備運動
-03. 円周率
-"Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
-'''
 s = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
 l = [len(i.strip(',.')) for i in s.split()]
 print(l)
