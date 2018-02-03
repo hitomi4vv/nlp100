60e78f94aed8a9fd7d4440d1e37cad889d59ceae
Hitomi <hitomi4vv@outlook.jp>
2018-02-03 19:05:08 +0900

    Add 03.py

--- 03.py
+++ 03.py
@@ -4,4 +4,5 @@
 "Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics."という文を単語に分解し，各単語の（アルファベットの）文字数を先頭から出現順に並べたリストを作成せよ．
 '''
 s = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
-print(s.replace(',', '').replace('.', '').split())
+l = [len(i.strip(',.')) for i in s.split()]
+print(l)
