92bd76c5ca227ebf74da2a6c49c5a4076d2225e3
Hitomi <hitomi4vv@outlook.jp>
2018-03-09 00:14:46 +0900

    wip

--- 04.py
+++ 04.py
@@ -1,3 +1,5 @@
 s = 'Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.'
-print(s.split())
+l = [i for i in s.split()]
+print(l)
 n = [1, 5, 6, 7, 8, 9, 15, 16, 19]
+print([(i + 1, v) for i, v in enumerate(n)])
