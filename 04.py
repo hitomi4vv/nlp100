b39fd1f2fccd8df08a75517275d6856ce07f1158
Hitomi <hitomi4vv@outlook.jp>
2018-03-13 00:01:23 +0900

    Cool

--- 04.py
+++ 04.py
@@ -1,4 +1,4 @@
 s = 'Hi He Lied Because Boron Could Not Oxidize Fluorine. New Nations Might Also Sign Peace Security Clause. Arthur King Can.'
 n = [1, 5, 6, 7, 8, 9, 15, 16, 19]
 l = s.split()
-print([[v[:1], i] if i in n else [v[:2], i] for i, v in enumerate(l, 1)])
+print([[v[:1] if i in n else v[:2], i] for i, v in enumerate(l, 1)])
