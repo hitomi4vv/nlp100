96ddd56dfb042e49064c068787f5f766fb5ab055
Hitomi Suzuki <hitomi4vv@outlook.jp>
2018-04-03 00:03:20 +0900

    wip

--- 08.py
+++ 08.py
@@ -1,5 +1,12 @@
 def cipher(s):
+  for c in s:
+    print(c)
   return s
 
 def decipher(s):
+  for c in s:
+    print(c)
   return s
+
+print(cipher('Hello world!'))
+print(decipher('Hello world!'))
