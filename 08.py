21b6893be448900bfce89e8695330a8e964e0860
Hitomi Suzuki <hitomi4vv@outlook.jp>
2018-04-08 17:53:37 +0900

    wip

--- 08.py
+++ 08.py
@@ -1,13 +1,7 @@
 def cipher(s):
-  for c in s:
-    if c.islower() and c.isalpha():
-        print(219 - ord(c))
-  return s
-
-def decipher(s):
-  for c in s:
-    print(chr(219 + 118))
-  return s
+    ss = ''
+    for c in s:
+        ss += chr(219 - ord(c)) if c.islower() and c.isalpha() else c
+    return ss
 
 print(cipher('Hello world!'))
-print(decipher('Hello world!'))
