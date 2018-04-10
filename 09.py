13b41fc6f76e4079ea1682f5d3787ca5e8a325ed
Hitomi Suzuki <hitomi4vv@outlook.jp>
2018-04-11 04:20:26 +0900

    wip

--- 09.py
+++ 09.py
@@ -1,4 +1,6 @@
 def typoglycemia(s):
+  l = [len(c) for c in s.split()]
+  print(l)
   return s.split()
 
 print(typoglycemia("I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."))
