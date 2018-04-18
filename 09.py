04bb34688253c7a0bd4e728c0043ee67420785d9
Hitomi Suzuki <hitomi4vv@outlook.jp>
2018-04-18 19:32:41 +0900

    wip

--- 09.py
+++ 09.py
@@ -2,7 +2,7 @@ import random
 
 def typoglycemia(s):
   l = s.split()
-  l = [random.shuffle(c.split()) if len(c) > 4 else c for c in l]
+  l = ['hoge' if len(c) > 4 else c for c in l]
   return ' '.join(l)
 
 s = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
