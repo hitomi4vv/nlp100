be2f472c44f507ae4069d32c7c190c4893509725
Hitomi Suzuki <hitomi4vv@outlook.jp>
2018-04-21 16:59:15 +0900

    Add 09.py

--- 09.py
+++ 09.py
@@ -1,8 +1,13 @@
 import random
 
+def shuffle(s):
+    l = list(s)
+    random.shuffle(l)
+    return ''.join(l)
+
 def typoglycemia(s):
   l = s.split()
-  return ' '.join([c[0] + c + c[len(c)-1] if len(c) > 4 else c for c in l])
+  return ' '.join([c[0] + shuffle(list(c)[1:len(c)-1]) + c[len(c)-1] if len(c) > 4 else c for c in l])
 
 s = "I couldn't believe that I could actually understand what I was reading : the phenomenal power of the human mind ."
 print(typoglycemia(s))
