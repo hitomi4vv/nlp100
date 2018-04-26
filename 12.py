54d741be6127da6bf77851684319eaa89801d717
Hitomi Suzuki <hitomi4vv@outlook.jp>
2018-04-27 02:10:37 +0900

    better

--- 12.py
+++ 12.py
@@ -1,9 +1,6 @@
-f = open('hightemp.txt')
-s = [l.split() for l in f.read().splitlines()]
-f.close()
+with open('hightemp.txt') as f:
+    s = [l.split() for l in f.read().splitlines()]
 with open('col1.txt', 'w') as f:
     f.write('\n'.join([i[0] for i in s]))
-f.close()
 with open('col2.txt', 'w') as f:
     f.write('\n'.join([i[1] for i in s]))
-f.close()
