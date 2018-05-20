afb83166b8f27ee5456bc890bf3998cc0bd81635
Hitomi Suzuki <hitomi4vv@outlook.jp>
2018-05-21 06:01:56 +0900

    wip

--- 13.py
+++ 13.py
@@ -1,2 +1,2 @@
 with open('col1.txt') as f1, open('col2.txt') as f2:
-  print([c1.strip('\n') + '\t' + c2 for c1, c2 in zip(f1, f2)])
+  print(''.join([c1.strip('\n') + '\t' + c2 for c1, c2 in zip(f1, f2)]))
