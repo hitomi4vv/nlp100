479b8daa427a60f2e5ff411eda42d91715df33cc
Hitomi Suzuki <hitomi4vv@outlook.jp>
2018-05-28 20:59:01 +0900

    wip

--- 14.py
+++ 14.py
@@ -1,5 +1,4 @@
 import sys
 argv = sys.argv
-print(argv)
 with open('out.txt') as f:
-    print(f.readlines()[:int(argv[1]) if len(argv) > 1 else -1])
+    print(''.join(f.readlines()[:int(argv[1]) if len(argv) > 1 else -1]))
