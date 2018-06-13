9ddb3a4688eb7cd232ccd63f6f7a9bfa0c65ac5f
Hitomi Suzuki <hitomi4vv@outlook.jp>
2018-06-14 03:27:22 +0900

    wip

--- 16.py
+++ 16.py
@@ -1,3 +1,6 @@
-import itertools
-with open('hightemp.txt') as f:
-  print(list(itertools.zip_longest(*[iter(f.readlines())]*10)))
+import sys, itertools
+with open('hightemp.txt') as f1:
+  l = list(itertools.zip_longest(*[iter(f1.readlines())]*10))
+  print(int(sys.argv[1]) if len(sys.argv) > 1 else 0)
+  print(len(l))
+  print('hightemp_{:02d}.txt'.format(4))
