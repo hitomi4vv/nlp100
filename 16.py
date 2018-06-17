0d49f1d24e3b3f6a2ac2e870bdceba01003c9177
Hitomi Suzuki <hitomi4vv@outlook.jp>
2018-06-18 07:09:12 +0900

    Add 16.py

--- 16.py
+++ 16.py
@@ -1,4 +1,4 @@
 import sys, itertools
-with open('hightemp.txt') as f1:
-  for i, j in enumerate(itertools.zip_longest(*[iter(f1.readlines())] * int(sys.argv[1]) if len(sys.argv) > 1 else len(f1.readlines()))):
+with open('hightemp.txt') as f:
+  for i, j in enumerate(itertools.zip_longest(*[iter(f.readlines())] * (int(sys.argv[1]) if len(sys.argv) > 1 else 1))):
     open('hightemp_{:02d}.txt'.format(i), 'w').writelines(''.join(filter(None, j)))
