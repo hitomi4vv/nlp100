1b35671a7e034a7f60eae0ac5ad35bb620e19d44
Hitomi Suzuki <hitomi4vv@outlook.jp>
2018-07-02 01:03:55 +0900

    wip

--- 19.py
+++ 19.py
@@ -1,2 +1,3 @@
+import collections
 with open('hightemp.txt') as f:
-    print('\n'.join(sorted([i[0] for i in [l.split() for l in f.read().splitlines()]])))
+    print(collections.Counter(sorted([i[0] for i in [l.split() for l in f.read().splitlines()]])).most_common())
