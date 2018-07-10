01f1150db1afc81869e66964dee0ffd5ffbce547
Hitomi Suzuki <hitomi4vv@outlook.jp>
2018-07-11 04:00:20 +0900

    Add 20.py

--- 20.py
+++ 20.py
@@ -1,4 +1,3 @@
 import json, gzip
 with gzip.open('jawiki-country.json.gz', 'rt') as f:
-    for line in f:
-        print(json.loads(line)['title'])
+    print('\n'.join([json.loads(l)['text'] for l in f.readlines() if json.loads(l)['title'] == 'イギリス']))
