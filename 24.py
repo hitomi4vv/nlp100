f54c2986b18f14effd7d701a8101cf124b19a99b
Hitomi Suzuki <hitomi4vv@outlook.jp>
2018-08-03 23:15:01 +0900

    wip

--- 24.py
+++ 24.py
@@ -1,4 +1,4 @@
 import json, gzip, re
 with gzip.open('jawiki-country.json.gz', 'rt') as f:
     text = '\n'.join([json.loads(l)['text'] for l in f.readlines() if json.loads(l)['title'] == 'イギリス'])
-print('\n'.join(re.compile(r'.*\[\[File:(.*)\]\].*').findall(text)))
+print('\n'.join(re.compile(r'.*\[\[File:(.*?)\|.*\]\].*').findall(text)))
