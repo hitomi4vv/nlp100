ad4231fee1685fe76f10c86c5cf2ee39bf8e04a8
Hitomi Suzuki <hitomi4vv@outlook.jp>
2018-08-09 22:36:21 +0900

    wip

--- 25.py
+++ 25.py
@@ -1,4 +1,4 @@
 import json, gzip, re
 with gzip.open('jawiki-country.json.gz', 'rt') as f:
     text = '\n'.join([json.loads(l)['text'] for l in f.readlines() if json.loads(l)['title'] == 'イギリス'])
-print('\n'.join(re.compile(r'^\|.*?\s*=\s*.*?$', re.MULTILINE).findall(text)))
+print(re.compile(r'^\|(.*?)\s*=\s*(.*?)$', re.MULTILINE).findall(text))
