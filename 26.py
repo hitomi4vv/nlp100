4851a32c30d1b78973ae3525a6e6eb5a610a9788
Hitomi Suzuki <hitomi4vv@outlook.jp>
2018-08-15 03:39:25 +0900

    Add 26.py

--- 26.py
+++ 26.py
@@ -2,4 +2,4 @@ import json, gzip, re
 with gzip.open('jawiki-country.json.gz', 'rt') as f:
     text = '\n'.join([json.loads(l)['text'] for l in f.readlines() if json.loads(l)['title'] == 'イギリス'])
 text = re.compile(r'^\{\{基礎情報.*?$.*?^\}\}$', re.MULTILINE + re.DOTALL).findall(text)[0]
-print(dict(re.compile(r'^\|(.*?)\s*=\s*(.*?)$', re.MULTILINE).findall(text)))
+print(dict(re.compile(r'^\|(.*?)\s*=\s*(.*?)$', re.MULTILINE).findall(re.compile(r'\'{2,5}').sub('', text))))
