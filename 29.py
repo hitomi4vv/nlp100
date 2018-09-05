d024356ddb393bea86ad492519651b0d17dc1268
Hitomi Suzuki <hitomi4vv@outlook.jp>
2018-09-05 22:21:12 +0900

    Add 29.py

--- 29.py
+++ 29.py
@@ -1,8 +1,12 @@
-import requests
-
+import json, gzip, re, requests
+with gzip.open('jawiki-country.json.gz', 'rt') as f:
+    text = '\n'.join([json.loads(l)['text'] for l in f.readlines() if json.loads(l)['title'] == 'イギリス'])
+text = re.compile(r'^\{\{基礎情報.*?$.*?^\}\}$', re.MULTILINE + re.DOTALL).findall(text)[0]
+pattern = r'\'{2,5}(.*?)(\1)|\[\[(?:[^:\]]+?\|)?|\]\]'
+d = dict(re.compile(r'^\|(.*?)\s*=\s*(.*?)$', re.MULTILINE).findall(re.compile(pattern).sub(r'\1', text)))
 print(requests.get('https://www.mediawiki.org/w/api.php', params = {
   'action': 'query',
-  'titles': 'File:Flag of the United Kingdom.svg',
+  'titles': 'File:' + d['国旗画像'],
   'format': 'json',
   'prop': 'imageinfo',
   'iiprop': 'url'
